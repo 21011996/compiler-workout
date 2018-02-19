90a36a0ce12f482678b4b5777caefe89659bb808
Ilya Kachalsky <ilya.kachalsky@gmail.com>
2018-02-19 20:47:10 +0300

    Expr done

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,13 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution. 
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"]*)
 
 (* Expression evaluator
 
@@ -50,5 +50,47 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let numOpToFunc name a b = 
+  let func = match name with
+    | "+" -> ( + )
+    | "-" -> ( - )
+    | "*" -> ( * )
+    | "/" -> (  / )
+    | "%" -> ( mod )
+    | _ -> failwith "Wrong numerical operator"
+  in func a b
+
+let cmpOpToFunc name a b = 
+  let func = match name with
+    | "<" -> ( < )
+    | ">" -> ( > )
+    | "<=" -> ( <= )
+    | ">=" -> ( >= )
+    | "==" -> ( = )
+    | "!=" -> ( != )
+    | _ -> failwith "Wrong compair operator"
+  in if func a b then 1 else 0
+
+let logicOpToFunc name a b = 
+  let func = match name with
+    | "&&" -> ( && )
+    | "!!" -> ( || )
+    | _ -> failwith "Wrong logic operator"
+  in 
+    let intToBool a = a != 0
+    in if func (intToBool a) (intToBool b) then 1 else 0
+
+let opSeparator name a b =
+  let func = match name with
+    | "+" | "-" | "*" | "/" | "%" -> numOpToFunc
+    | "<" | ">" | "<=" | ">=" | "==" | "!=" -> cmpOpToFunc
+    | "&&" | "!!" -> logicOpToFunc
+    | _ -> failwith "Wrong operator"
+  in func name a b
+
+let eval state expr = 
+  let rec recEval state expr = match expr with
+    | Const value -> value
+    | Var name -> state name
+    | Binop (operator,left,right) -> opSeparator operator (recEval state left) (recEval state right)
+  in recEval state expr                  
